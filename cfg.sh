5cbb36e53b5e19c2d4748ed219cc74011ea89285
Shaun Lloyd <dev.lloydie@gmail.com>
2019-01-11 15:23:35 +1100

    final clean up

--- cfg.sh
+++ cfg.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -d ~/.Config ]; then
-    echo yes
-else
-    echo no
-fi
-
 cfg-bash-update() {
 	find ~/.Config/bash/{bashrc,alias}/ -type f -executable | sort | xargs -L1 cat > ~/.bashrc
 	find /usr/local/helper -type f -executable | xargs -L1 cat >> ~/.bashrc
@@ -25,21 +19,37 @@ cfg-tmux-tpm-setup() {
 cfg-vim-update() {
 	find ~/.Config/vim/vimrc -type f -executable | sort | xargs -L 1 cat > ~/.vimrc
 	find ~/.Config/vim/plugin -type f -executable | sort | xargs -L 1 cat >> ~/.vimrc
+	cfg-vim-plugin-update
 }
 
-cfg-vim-vundle-update() {
-    vim +PluginInstall +q +q
+cfg-vim-plugin-update() {
+    vim +PluginInstall +PluginUpdate +q +q
 }
 
-cfg-vim-vundle-install() {
-	mkdir -p ~/.vim/bundle
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cfg-vim-plugin-setup() {
+    if [ -d ~/.vim/bundle ]; then
+	echo 'bundle dir exists.'
+	read -p "delete ? "
+	if [ "$REPLY" == 'y' ]; then
+	    rm -r ~/.vim/bundle/*
+	else
+	    exit
+	fi
+    fi
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 }
 
 
 
-setup() {
-    ln -s "$CFG_PATH/vim/vim" "$HOME/.vim"
+
+cfg-vim-plugin-youcompleteme-setup() {
     cd ~/.vim/bundle/YouCompleteMe || exit
     python3 install.py --all
 }
+
+if [ -d ~/.Config ]; then
+    echo yes
+else
+    git clone https://github.com/lloydie/Config.git ~/.Config
+fi
+
